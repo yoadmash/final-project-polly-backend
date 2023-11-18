2e4c30704306be932392f9b843c3c91b88b7ce46
yoadmash <yoad.mashiach2@gmail.com>
2023-11-18 12:49:23 +0200

    connecting to live data base

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.LOCAL_DATABASE, {
+        await mongoose.connect(process.env.DATABASE_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
